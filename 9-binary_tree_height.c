5006f49b104dcae25c3766f65165891ce96b1845
iyiolakeni <iyiolakeni@gmail.com>
2023-08-31 20:00:46 +0000

    Betty compliant

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Height measurement of a binary tree
+ * binary_tree_height- Height measurement of a binary tree
  *
  * @tree: a pointer to the tree's root node, which must be traversed
+ *
+ * Return: Height of the tree, return 0 if tre is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -16,6 +18,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height) ? 
+	return ((left_height > right_height) ?
 			left_height + 1 : right_height + 1);
 }
