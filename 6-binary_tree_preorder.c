1fdee93877a60ae44dd94b815390ac48460fc35a
iyiolakeni <iyiolakeni@gmail.com>
2023-08-31 19:17:26 +0000

    Another try

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,10 +8,10 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	while (tree)
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		tree = tree->right;
-	}
+	if (tree == NULL || func == NULL)
+		return;
+	
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
