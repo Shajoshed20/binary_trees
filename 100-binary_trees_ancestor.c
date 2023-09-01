4a72f7d51aa039bfc96b92f57570ead596c463cd
iyiolakeni <iyiolakeni@gmail.com>
2023-09-01 19:35:29 +0000

    another

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,43 +10,26 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	const binary_tree_t *pop;
+	const binary_tree_t *popp;
+
 	if (!first || !second)
 		return (NULL);
 
-	/* Create an array to store root path to first node */
-	binary_tree_t *path_first[100];
-	int path_length_first = 0;
+	pop = first;
 
-	/* Create an array to store root path to the second node */
-	binary_tree_t *path_two[100];
-	int path_length_two = 0;
-
-	binary_tree_t *current_first = (binary_tree_t *)frist;
-	binary_tree_t *current_second = (binary_tree_t *)second;
-
-	while (current_first)
+	while (pop)
 	{
-		path_first[path_length_first] = current_first;
-		path_length_first++;
-		current_first = current_first->parent;
+		popp = second;
+
+		while (popp)
+		{
+			if (pop == popp)
+				return ((binary_tree_t *)pop);
+			popp = popp->parent;
+		}
+		pop = pop->parent;
 	}
 
-	while (current_second)
-	{
-		path_two[path_length_two] = current_second;
-		path_length_second++;
-		current_second = current_second->parent;
-	}
-
-	binary_tree_t *ancestor = NULL;
-
-	while (path_length_first > 0 && path_length_two > 0 &&
-			path_first[path_length_first - 1] == path_two[path_length_two - 1])
-	{
-		ancestor = path_first[path_length_first - 1];
-		path_length_first--;
-		path_length_second--;
-	}
-
-	return (ancestor);
+	return (NULL);
 }
