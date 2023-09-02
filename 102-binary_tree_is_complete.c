5a3538f2a51d85d3312b91ec10224ae8982d05d2
iyiolakeni <iyiolakeni@gmail.com>
2023-09-02 00:15:50 +0000

    try

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,8 +1,51 @@
 #include "binary_trees.h"
+static queue_node_t *queue_push(queue_node_t *rear, const binary_tree_t *node);
+static queue_node_t *queue_pop(queue_node_t *front);
+/**
+ * queue_push - Enqueues a binary tree node.
+ * @rear: A pointer to the rear of the queue.
+ * @node: A pointer to the binary tree node to enqueue.
+ * Return: A pointer to the new rear of the queue.
+ */
+static queue_node_t *queue_push(queue_node_t *rear, const binary_tree_t *node)
+{
+	queue_node_t *new_node = malloc(sizeof(queue_node_t));
+
+	if (!new_node)
+	{
+		perror("Memory allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->node = node;
+	new_node->next = NULL;
+
+	if (!rear)
+		return (new_node);
+
+	rear->next = new_node;
+	return (new_node);
+}
+
+/**
+ * queue_pop - Dequeues a binary tree node.
+ * @front: A pointer to the front of the queue.
+ * Return: A pointer to the new front of the queue.
+ */
+static queue_node_t *queue_pop(queue_node_t *front)
+{
+	if (!front)
+		return (NULL);
+
+	queue_node_t *next_node = front->next;
+
+	free(front);
+	return (next_node);
+}
+
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete.
- * @tree: Pointer to the root node of the tree to check.
- *
+ * @tree: A pointer to the root node of the tree to check.
  * Return: 1 if the tree is complete, 0 otherwise.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
@@ -10,35 +53,30 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	int a = 1;
-	int start = 0;
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1000);
-	int head = 0;
-	int tail = 0;
+	queue_node_t *queue = NULL;
+	int level_started = 0;
 
-	queue[tail++] = (binary_tree_t *) tree;
+	queue = queue_push(queue, tree);
 
-	while (head < tail)
+	while (queue)
 	{
-		binary_tree_t *now = queue[head++];
+		const binary_tree_t *current = queue->node;
 
-		if (!now)
+		queue = queue_pop(queue);
+
+		if (!current)
 		{
-			start = 1;
+			level_started = 1;
 		}
 		else
 		{
-			if (start)
-			{
-				a = 0;
-				break;
-			}
+			if (level_started)
+				return (0);
 
-			queue[tail++] = now->left;
-			queue[tail++] = now->right;
+			queue = queue_push(queue, current->left);
+			queue = queue_push(queue, current->right);
 		}
 	}
 
-	free(queue);
-	return (a);
+	return (1);
 }
